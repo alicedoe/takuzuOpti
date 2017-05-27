e0167d82681ef5ba7c130e6e716eae7804d8be49
alice.edn@gmail.com <alice.edn@gmail.com>
2017-05-27 21:12:13 +0200

    readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# takuzuOpti
+# Générateur de grille de takuzu 8x8
+
+-[X] algorithme pour générer une grille de 8 x 8 respectant les régèles du takuzu
+-[X] améliorer la 1ère version qui était trop en brute force
+-[X] PHP 7.0
--- takuzuopticonsole.php
+++ takuzuopticonsole.php
@@ -1,6 +1,7 @@
 <?php
 
 include 'functions.php';
+$timestamp_debut = microtime(true);
 
 //grid() tant qu'elle est fausse pour créer la grille de 8x8 respectant les règles du takuzu
 do {
@@ -8,3 +9,7 @@ do {
 } while ($grille == false);
 
 displayGridTest($grille);
+
+$timestamp_fin = microtime(true);
+$difference_ms = $timestamp_fin - $timestamp_debut;
+echo 'Exécution du script : ' . $difference_ms . ' secondes.';
